8d7bcb5f902f6ef9d8dd666294651874f83f9e96
Sunnary2604 <50614684+Sunnary2604@users.noreply.github.com>
2021-03-24 10:51:10 +0800

    Update WebSecurityConfig.java

--- T-Manager/src/main/java/cpt202/groupwork/security/WebSecurityConfig.java
+++ T-Manager/src/main/java/cpt202/groupwork/security/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.headers().disable();
-//    http.cors();
+    http.cors();
 
   }
 }
