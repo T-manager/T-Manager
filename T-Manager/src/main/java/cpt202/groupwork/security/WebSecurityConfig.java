5f88f9a82389d8bf81234bea7915c5ae4fd9cef2
Sunnary2604 <50614684+Sunnary2604@users.noreply.github.com>
2021-03-24 10:48:22 +0800

    security config

---
+++ T-Manager/src/main/java/cpt202/groupwork/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package cpt202.groupwork.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+// 允许Spring查找该类并将其自动应用于全局Web安全
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+    // securedEnabled = true,
+    // jsr250Enabled = true,
+    prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.headers().disable();
+//    http.cors();
+
+  }
+}
