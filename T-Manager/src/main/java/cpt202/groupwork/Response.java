c6663eed1f6cc53c79ccda2aaa03a9a1feca5c28
orion <zhonghao.wang18@student.xjtlu.edu.cn>
2021-04-07 17:26:35 +0800

    添加User增删改查，用户注册登录。将security与token生成、验证完成。现在只允许api/auth/**可以无登录请求。测试时可在config包中修改

--- T-Manager/src/main/java/cpt202/groupwork/Response.java
+++ T-Manager/src/main/java/cpt202/groupwork/Response.java
@@ -57,6 +57,19 @@ public class Response<T> {
     public static Response<?> permissionDenied(String message) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN, message);
     }
+    // 未通过
+    public static Response<?> fail() {
+        return Response.builder().status(HttpStatus.OK.value()).message("Fail").data(new ArrayList<>()).build();
+    }
 
+    // 未通过 + 携带相关 data 数据
+    public static Response<?> fail(Object data) {
+        return Response.builder().status(HttpStatus.OK.value()).message("Fail").data(data).build();
+    }
+
+    // 未通过 + 携带相关 data 数据
+    public static Response<?> fail(String message, Object data) {
+        return Response.builder().status(HttpStatus.OK.value()).message(message).data(data).build();
+    }
 
 }
---
+++ T-Manager/src/main/java/cpt202/groupwork/service/UserService.java
@@ -0,0 +1,24 @@
+package cpt202.groupwork.service;
+
+import cpt202.groupwork.Response;
+import cpt202.groupwork.entity.User;
+
+public interface UserService {
+
+  Response<?> userIdExists(final Integer userId);
+
+  Response<?> userNameExists(final String username);
+
+  Response<?> userCreate(final User user);
+
+  Response<?> userDelete(final Integer userId);
+
+  Response<?> userDelete(final String username);
+
+  Response<?> userModify(final Integer userId,  final User userMod);
+
+  Response<?> userModify(final String username,  final User userMod);
+
+  Response<?> userLogin(final User postUser);
+
+}
