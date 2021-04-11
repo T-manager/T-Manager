fb5e3c4d3882dd71825b9478a031f7ea41fea9dd
Carina0809 <kaiyuan.yang18@student.xjtlu.edu.cn>
2021-04-11 22:55:57 +0800

    Update index.js

--- tm-frontend/src/router/index.js
+++ tm-frontend/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import todolist from "@/pages/todolist";
 import test from "@/pages/test";
+import Gante from "@/pages/Gante";
+
 
 
 Vue.use(Router);
@@ -23,6 +25,12 @@ export default new Router({
       path: "/test",
       name: "test",
       component: test
+      
+    },
+    {
+      path: "/gante",
+      name: "gante",
+      component: Gante
       
     }
   ]
