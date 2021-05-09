5a3c2b10db984665560a249eab9be7e5bdca70b3
EckCiting <EckCiting@outlook.com>
2021-05-09 19:38:38 +0800

    统一"忘记密码"的dialog样式

--- tm-frontend/src/components/forgetPwdDialog.vue
+++ tm-frontend/src/components/forgetPwdDialog.vue
@@ -6,144 +6,169 @@
     >
       Forget password?
     </div>
-    <v-dialog v-model="showBeginReset" persistent max-width="500px">
-      <v-row no-gutters>
-        <v-col cols="12">
-          <v-form v-model="valid">
-            <v-card ref="form">
-              <v-card-title>Input your username</v-card-title>
-              <v-card-text>
-                <v-text-field
-                  outlined
-                  ref="userName"
-                  v-model="userName"
-                  :rules="[rules.required]"
-                  label="Enter username"
-                  color="primary"
-                  style="margin-top: 8px"
-                ></v-text-field>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn text @click="cancel"> Cancel </v-btn>
-                <v-spacer></v-spacer>
-                <v-btn
-                  :disabled="!this.valid"
-                  color="primary"
-                  @click="checkExistUser"
-                >
-                  NEXT
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-form>
-        </v-col>
-      </v-row>
+    <!-- Dialog for checking whether the username exists -->
+    <v-dialog v-model="showBeginReset" persistent max-width="450px">
+      <v-form v-model="valid">
+        <v-card class="resetPwdCard" ref="form">
+          <div style="display: flex; font-size: 30px; margin-left: 10px">
+            Input your username
+          </div>
+          <v-card-text style="margin-top: 30px; padding: 10px">
+            <v-text-field
+              outlined
+              ref="userName"
+              v-model="userName"
+              :rules="[rules.required]"
+              label="Enter username"
+              color="primary"
+            ></v-text-field>
+          </v-card-text>
+          <div style="display: flex; justify-content: center; margin-top: 10px">
+            <v-btn
+              @click="cancel"
+              depressed
+              style="
+                border: #cccccc solid 1px;
+                color: #777777;
+                margin-right: 50px;
+                width: 100px;
+              "
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              :disabled="!this.valid"
+              color="primary"
+              @click="checkExistUser"
+              style="color: #fff; width: 100px"
+              depressed
+            >
+              NEXT
+            </v-btn>
+          </div>
+        </v-card>
+      </v-form>
     </v-dialog>
+    <!-- Dialog for checking whether email matches username -->
     <v-dialog v-model="showVerifyInfo" persistent max-width="500px">
-      <v-row no-gutters>
-        <v-col cols="12">
-          <v-form v-model="valid">
-            <v-card ref="form">
-              <v-card-title>Input your Email address</v-card-title>
-              <v-card-text>
-                <v-text-field
-                  outlined
-                  ref="userEmail"
-                  v-model="userEmail"
-                  :rules="[rules.required, rules.email]"
-                  label="Enter Email address"
-                  color="primary"
-                  style="margin-top: 8px"
-                ></v-text-field>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn text @click="cancel"> Cancel </v-btn>
-                <v-spacer></v-spacer>
-                <v-btn
-                  :disabled="!this.valid"
-                  color="primary"
-                  @click="checkUserInfo"
-                  :loading="loading"
-                >
-                  NEXT
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-form>
-        </v-col>
-      </v-row>
+      <v-form v-model="valid">
+        <v-card class="resetPwdCard" ref="form">
+          <div style="display: flex; font-size: 30px; margin-left: 10px">
+            Input your Email address
+          </div>
+          <v-card-text style="margin-top: 30px; padding: 10px">
+            <v-text-field
+              outlined
+              ref="userEmail"
+              v-model="userEmail"
+              :rules="[rules.required, rules.email]"
+              label="Enter Email address"
+              color="primary"
+              style="margin-top: 8px"
+            ></v-text-field>
+          </v-card-text>
+          <div style="display: flex; justify-content: center; margin-top: 10px">
+            <v-btn
+              @click="cancel"
+              depressed
+              style="
+                border: #cccccc solid 1px;
+                color: #777777;
+                margin-right: 50px;
+                width: 100px;
+              "
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              :disabled="!this.valid"
+              :loading="loading"
+              color="primary"
+              @click="checkUserInfo"
+              style="color: #fff; width: 100px"
+              depressed
+            >
+              NEXT
+            </v-btn>
+          </div>
+        </v-card>
+      </v-form>
     </v-dialog>
+    <!-- Dialog for verification code -->
     <v-dialog v-model="showVerifyCode" persistent max-width="500px">
-      <v-row no-gutters>
-        <v-col cols="12">
-          <v-form v-model="valid">
-            <v-card ref="form">
-              <v-card-title
-                >Input the code recieved from your E-mail</v-card-title
+      <v-form v-model="valid">
+        <v-card ref="form" class="resetPwdCard">
+          <div style="display: flex; font-size: 30px; margin-left: 10px">
+            Input the verification code
+          </div>
+          <v-card-text style="margin-top: 30px; padding: 10px">
+            <v-text-field
+              outlined
+              ref="userVerifyCode"
+              v-model="userVerifyCode"
+              :rules="[rules.required, rules.code]"
+              label="Enter verification code"
+              color="primary"
+            ></v-text-field>
+            <div
+              style="
+                width: 100%;
+                display: flex;
+                justify-content: flex-end;
+                margin-top: -10px;
+              "
+            >
+              <div
+                style="
+                  text-decoration: underline;
+                  color: #6271c2;
+                  cursor: pointer;
+                "
+                @click="getVerifyCode"
               >
-              <v-card-text>
-                <v-text-field
-                  outlined
-                  ref="userVerifyCode"
-                  v-model="userVerifyCode"
-                  :rules="[rules.required, rules.code]"
-                  label="Enter verification code"
-                  color="primary"
-                  style="margin-top: 8px"
-                ></v-text-field>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn text @click="cancel"> Cancel </v-btn>
-                <v-spacer></v-spacer>
-                <div
-                  style="
-                    text-decoration: underline;
-                    color: #6271c2;
-                    cursor: pointer;
-                  "
-                  @click="getVerifyCode"
-                >
-                  Resend code
-                </div>
-                <v-spacer></v-spacer>
-                <v-btn
-                  :disabled="!this.valid"
-                  color="primary"
-                  @click="checkVerifyCode"
-                >
-                  NEXT
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-form>
-        </v-col>
-      </v-row>
+                Resend code
+              </div>
+            </div>
+          </v-card-text>
+          <div style="display: flex; justify-content: center; margin-top: 10px">
+            <v-btn
+              @click="cancel"
+              depressed
+              style="
+                border: #cccccc solid 1px;
+                color: #777777;
+                margin-right: 50px;
+                width: 100px;
+              "
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              :disabled="!this.valid"
+              :loading="loading"
+              color="primary"
+              @click="checkVerifyCode"
+              style="color: #fff; width: 100px"
+              depressed
+            >
+              NEXT
+            </v-btn>
+          </div>
+        </v-card>
+      </v-form>
     </v-dialog>
-    <v-dialog v-model="showModifyPwd" persistent max-width="600px">
-      <div
-        style="
-          height: 400px;
-          background-color: #ffffff;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        "
-      >
+    <!-- Dialog for modifying password -->
+    <v-dialog v-model="showModifyPwd" persistent max-width="500px">
+      <v-card style="padding: 30px 35px 50px 35px; border-radius: 10px">
         <div
           @click="showModifyPwd = false"
-          style="
-            display: flex;
-            justify-content: flex-end;
-            margin-top: 5px;
-            height: 30px;
-            width: 570px;
-          "
+          style="display: flex; justify-content: flex-end; width: 100%"
         >
           <v-icon style="font-size: 26px; cursor: pointer"> mdi-close</v-icon>
         </div>
         <div
           style="
-            font-size: 24px;
+            font-size: 30px;
             color: #101010;
             margin-top: -10px;
             width: 520px;
@@ -152,63 +177,42 @@
         >
           Edit Password
         </div>
-        <div
-          style="
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            margin-top: 20px;
-          "
-        >
+        <v-card-text style="margin-top: 30px; padding: 10px">
           <v-text-field
-            style="margin-top: 20px; width: 375px"
             outlined
             label="New password"
             v-model="newPassword"
-            :rules="[rules.required]"
+            :rules="[rules.required, rules.validChar, rules.length]"
             type="password"
           ></v-text-field>
           <v-text-field
             outlined
             label="Confirmation"
-            style="width: 375px"
             v-model="confirmPassword"
-            :rules="[rules.required]"
+            :rules="rules.required"
             type="password"
           ></v-text-field>
-        </div>
-        <div
-          class="d-flex justify-center"
-          style="font-color: #101010; font-size: 16px; margin-top: 39px"
-        >
+        </v-card-text>
+        <div style="display: flex; justify-content: center; margin-top: 10px">
+          <!-- close popup -->
           <v-btn
-            style="
-              margin-left: 194pxl;
-              margin-right: 194px;
-              width: 94px;
-              height: 39px;
-              border: 1px solid rgba(187, 187, 187, 100);
-              border-radius: 10px;
-            "
             depressed
-            outlined
+            style="border: #cccccc solid 1px; color: #777777; width: 100px"
             @click="showModifyPwd = false"
             >CANCLE</v-btn
           >
-          <v-spacer></v-spacer>
+          <!-- submit modification -->
           <v-btn
-            style="width: 94px; height: 39px; border-radius: 10px"
             depressed
             color="primary"
+            style="color: #fff; width: 100px; margin-left: 50px"
             :loading="loading"
             :disabled="loading"
             @click="submit"
             >SUBMIT</v-btn
           >
         </div>
-      </div>
+      </v-card>
     </v-dialog>
   </div>
 </template>
@@ -230,20 +234,20 @@ export default {
       confirmPassword: "",
       loading: false,
       rules: {
-        required: value => !!value || "This field is required.",
-        email: value => {
+        required: (value) => !!value || "This field is required.",
+        email: (value) => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || "Invalid e-mail.";
         },
-        code: value => {
+        code: (value) => {
           const pattern = /^\d{6}$/;
           return pattern.test(value) || "6 digits only";
-        }
-      }
+        },
+      },
     };
   },
   methods: {
-    cancel: function() {
+    cancel: function () {
       this.userName = "";
       this.userEmail = "";
       this.userVerifyCode = "";
@@ -254,24 +258,24 @@ export default {
       this.showVerifyCode = false;
       this.showModifyPwd = false;
     },
-    next1: function() {
+    next1: function () {
       this.showBeginReset = false;
       this.showVerifyInfo = true;
     },
-    next2: function() {
+    next2: function () {
       this.showVerifyInfo = false;
       this.showVerifyCode = true;
     },
-    next3: function() {
+    next3: function () {
       this.showVerifyCode = false;
       this.showModifyPwd = true;
     },
-    checkExistUser: async function() {
+    checkExistUser: async function () {
       this.$axios({
         method: "get",
-        url: this.$store.state.host + "auth/check?username=" + this.userName
+        url: this.$store.state.host + "auth/check?username=" + this.userName,
       })
-        .then(res => {
+        .then((res) => {
           if (res.data.data == 2000) {
             this.next1();
           } else {
@@ -279,21 +283,21 @@ export default {
             this.userName = null;
           }
         })
-        .catch(error => {
+        .catch((error) => {
           // console.log(error);
         });
     },
-    checkUserInfo: async function() {
+    checkUserInfo: async function () {
       (this.loading = true),
         this.$axios({
           method: "post",
           url: this.$store.state.host + "auth/check",
           data: {
             userName: this.userName,
-            userEmail: this.userEmail
-          }
+            userEmail: this.userEmail,
+          },
         })
-          .then(res => {
+          .then((res) => {
             if (res.data.data == 2000) {
               this.getVerifyCode();
             } else {
@@ -301,20 +305,20 @@ export default {
               this.userEmail = null;
             }
           })
-          .catch(error => {
+          .catch((error) => {
             // console.log(error);
           });
     },
-    getVerifyCode: async function() {
+    getVerifyCode: async function () {
       this.$axios({
         method: "post",
         url: this.$store.state.host + "auth/codesending",
         data: {
           userName: this.userName,
-          userEmail: this.userEmail
-        }
+          userEmail: this.userEmail,
+        },
       })
-        .then(res => {
+        .then((res) => {
           if (res.data.data == 3000) {
             this.loading = false;
             this.next2();
@@ -323,28 +327,28 @@ export default {
             this.userEmail = null;
           }
         })
-        .catch(error => {
+        .catch((error) => {
           // console.log(error);
         });
     },
-    checkVerifyCode: async function() {
+    checkVerifyCode: async function () {
       this.$axios({
         method: "post",
         url: this.$store.state.host + "auth/codeVerification",
         data: {
           userName: this.userName,
           userEmail: this.userEmail,
-          verifyPassword: this.userVerifyCode
-        }
+          verifyPassword: this.userVerifyCode,
+        },
       })
-        .then(res => {
+        .then((res) => {
           if (res.data.data == "Verify successful") {
             this.next3();
           } else {
             alert(res.data.message);
           }
         })
-        .catch(error => {
+        .catch((error) => {
           // console.log(error);
         });
     },
@@ -368,28 +372,32 @@ export default {
           method: "put",
           url:
             this.$store.state.host +
-            "auth/edit/" + // ！！修改密码未完善，此处无需token认证即可修改密码
+            "auth/edit/" + //修改密码未完善，此处无需token认证即可修改密码
             this.userName,
           data: {
             userName: this.userName,
-            userPassword: this.userPassword
-          }
+            userPassword: this.userPassword,
+          },
         })
-          .then(res => {
+          .then((res) => {
             alert("Reset password successfully!");
             this.$store.commit("del_username");
             this.$store.commit("del_token");
             this.$router.go(0);
           })
-          .catch(error => {
+          .catch((error) => {
             // console.log(error);
             //   this.$store.commit("response", error);
             this.loading = false;
           });
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.resetPwdCard {
+  padding: 30px 35px 50px 35px;
+}
+</style>
