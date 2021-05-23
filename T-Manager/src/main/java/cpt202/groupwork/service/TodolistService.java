0dae2da18628f0e95315081f20ac43fdbd263089
Sunnary2604 <50614684+Sunnary2604@users.noreply.github.com>
2021-05-23 15:50:23 +0800

    Get todolist Exception handling

--- T-Manager/src/main/java/cpt202/groupwork/service/TodolistService.java
+++ T-Manager/src/main/java/cpt202/groupwork/service/TodolistService.java
@@ -1,11 +1,12 @@
 package cpt202.groupwork.service;
 
+import cpt202.groupwork.Response;
 import cpt202.groupwork.dto.TodolistViewDTO;
 import java.util.List;
 
 public interface TodolistService {
 
-  List<TodolistViewDTO> getTodolist(Integer projectId);
+  Response<?> getTodolist(Integer projectId);
 
   List<TodolistViewDTO> searchTodos(Integer projectId, String todoName);
 }
